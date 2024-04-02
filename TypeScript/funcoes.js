02441b175aa74ecf1602121ef550d7166af628c3
Pedro <pfdsa1@hotmail.com>
2024-04-01 21:46:06 -0300

    first commit

---
+++ TypeScript/funcoes.js
@@ -0,0 +1,12 @@
+"use strict";
+function calculaArea(base, altura) {
+    return base * altura;
+}
+function multiplica(numero1, numero2) {
+    return numero1 * numero2;
+}
+console.log(multiplica(5, 10));
+function saudacao(nome) {
+    return `Olá ${nome}`;
+}
+console.log(saudacao('Pedro'));
---
+++ TypeScript/funcoes.ts
@@ -0,0 +1,12 @@
+
+function multiplica(numero1:number, numero2: number): number{
+    return numero1*numero2;
+}
+
+console.log(multiplica(5, 10));
+
+function saudacao(nome:string):string{
+    return `Olá ${nome}`
+}
+
+console.log(saudacao('Pedro'));
